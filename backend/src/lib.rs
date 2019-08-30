5d6efbb1b8db33debc9c69914a82c3892e4b5b7f
bofeiw <bofei@mail.com>
2019-08-31 00:35:43 +1000

    implement backend

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -3,84 +3,72 @@ use smart_contract_macros::smart_contract;
 use smart_contract::log;
 use smart_contract::payload::Parameters;
 use std::collections::VecDeque;
+use std::collections::HashMap;
 
-struct Todo {
-    content: String,
-    done: bool
-}
-
-struct TodoList {
-    logs: VecDeque<Todo>
-}
-
-const MAX_LOG_CAPACITY: usize = 20;
-const MAX_MESSAGE_SIZE: usize = 250;
-
-fn prune_old_todos(list: &mut TodoList) {
-    if list.logs.len() > MAX_LOG_CAPACITY {
-        list.logs.pop_front();
-    }
+struct BallotPaper {
+    votes: HashMap<String, u32>,
+    voted_ids: VecDeque<[u8; 32]>,
 }
 
 #[smart_contract]
-impl TodoList {
+impl BallotPaper {
     fn init(_params: &mut Parameters) -> Self {
-        Self { logs: VecDeque::new() }
+        let mut votes: HashMap<String, u32> = HashMap::new();
+
+        votes.insert("a".to_string(), 0);
+        votes.insert("b".to_string(), 0);
+        votes.insert("c".to_string(), 0);
+        votes.insert("d".to_string(), 0);
+        votes.insert("e".to_string(), 0);
+
+        Self {
+            votes: votes,
+            voted_ids: VecDeque::new(),
+        }
     }
 
-    fn add_todo(&mut self, params: &mut Parameters) -> Result<(), String> {
-        let todo = Todo { content: params.read(), done: false };
+    fn is_voted(&mut self, params: &mut Parameters) -> Result<(), String> {
+        let id: [u8; 32] = params.sender;
+        let voted: bool = self.voted_ids.contains(&id);
 
-        // Ensure that todo contents are not empty.
-        if todo.content.len() == 0 {
-            return Err("Message must not be empty.".into());
+        if voted {
+            log("1");
+        } else {
+            log("0");
         }
-
-        // Ensure that content are at most 240 characters.
-        if todo.content.len() > MAX_MESSAGE_SIZE {
-            return Err(format!("Message must not be more than {} characters.", MAX_MESSAGE_SIZE).into());
-        }
-
-        // Push todo into list.
-        self.logs.push_back(todo);
-
-        // Prune old todos if necessary.
-        prune_old_todos(self);
 
         Ok(())
     }
 
-    fn remove_todo(&mut self, params: &mut Parameters) -> Result<(), String> {
-        let target:usize = params.read();
-        if target < self.logs.len() {
-            self.logs.remove(target);
+    fn vote(&mut self, params: &mut Parameters) -> Result<(), String> {
+        let id: [u8; 32] = params.sender;
+        let voted: bool = self.voted_ids.contains(&id);
+
+        if voted {
+            return Err(format!("No duplicated vote."));
+        } else {
+            self.voted_ids.push_back(id);
         }
-        else {
-            return Err("Index out of bounds.".into());
+
+        for preference in 1u32..6u32 {
+            let candidate: String = params.read();
+            match self.votes.get_mut(&candidate) {
+                Some(v) => *v += preference,
+                None => {}
+            }
         }
+
         Ok(())
     }
 
-    fn toggle_todo(&mut self, params: &mut Parameters) -> Result<(), String> {
-        let target:usize = params.read();
-        if target < self.logs.len() {
-            let target_ref = &mut self.logs[target];
-            target_ref.done = !target_ref.done;
-        }
-        else {
-            return Err("Index out of bounds.".into());
-        }
-        Ok(())
-    }
+    fn get_votes(&mut self, _params: &mut Parameters) -> Result<(), String> {
+        let mut votes = Vec::new();
 
-    fn get_todos(&mut self, _params: &mut Parameters) -> Result<(), String> {
-        let mut todos = Vec::new();
-
-        for todo in &self.logs {
-            todos.insert(0, format!("<{}> {}", todo.content, todo.done));
+        for (k, v) in self.votes.iter() {
+            votes.insert(0, format!("{}:{}", k, v));
         }
 
-        log(&todos.join("\n"));
+        log(&votes.join(";"));
 
         Ok(())
     }
