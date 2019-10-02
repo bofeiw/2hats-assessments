f3fe18a983ff145aa5c8e5603135297e5b31b0ef
bofeiw <bofei@mail.com>
2019-10-02 21:52:28 +1000

    init react

---
+++ react-web-dev/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+
+function App() {
+    return (
+        <Box width="100%" height="128px" bgcolor="#6200ee">
+          test
+        </Box>
+    );
+}
+
+export default App;
